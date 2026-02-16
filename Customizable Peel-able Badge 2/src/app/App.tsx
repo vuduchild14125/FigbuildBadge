77e10920ed06a4fffdcf4a30c9dd9f90081cb50a
Abby Hart <axh2451@rit.edu>
2026-02-16 16:06:48 -0500

    added left align, deleted cards

--- Customizable Peel-able Badge 2/src/app/App.tsx
+++ Customizable Peel-able Badge 2/src/app/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import { useState, useRef } from 'react';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 
